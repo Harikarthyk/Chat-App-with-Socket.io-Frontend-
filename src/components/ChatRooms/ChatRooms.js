2218738d24de67a82420f2548a17d0590e5a45cd
Hari_karthyk <Harikarthyk@users.noreply.github.com>
2020-12-25 07:58:15 +0530

    v-1

--- src/components/ChatRooms/ChatRooms.js
+++ src/components/ChatRooms/ChatRooms.js
@@ -1,5 +1,5 @@
 import React, {useContext, useState} from "react";
-import {FaArrowRight, FaPlus} from "react-icons/fa";
+import {FaPlus} from "react-icons/fa";
 import {Redirect} from "react-router-dom";
 import context from "../../context/context";
 import Modal, {closeStyle} from "simple-react-modal";
@@ -10,7 +10,6 @@ import Error from "../Error/Error";
 function ChatRooms() {
 	const {user} = useContext(context);
 	const [input, setIntput] = useState({
-		message: "",
 		chatRoom: "",
 	});
 	const [output, setOutput] = useState({
@@ -18,16 +17,21 @@ function ChatRooms() {
 		message: "",
 	});
 	const [showModel, setShowModel] = useState(false);
-	const {message, chatRoom} = input;
+	const {chatRoom} = input;
 	if (!user) return <Redirect to='/' />;
 	const handleChatNameListener = () => {
-		createChatRoom({roomName: chatRoom}, user.token, user.user._id)
+		createChatRoom(
+			{roomName: chatRoom, allUsers: [{user: user.user._id}]},
+			user.token,
+			user.user._id,
+		)
 			.then((result) => {
 				if (result.error) {
 					setOutput({...output, message: result.error, error: true});
 					return;
 				}
-				setShowModel(false);
+                setShowModel(false);
+                
 			})
 			.catch((error) => console.error(error));
 	};
@@ -35,69 +39,13 @@ function ChatRooms() {
 	return (
 		<>
 			<div className='ChatRooms'>
-				<div className='ChatRooms__left'>
-					<div
-						className='ChatRooms__left__title'
-						style={{
-							background: "#ce1111",
-							color: "white",
-							fontWeight: "bolder",
-						}}
-					>
-						Group 01
-					</div>
-					<div className='ChatRooms__left__title'>Group 02</div>
-					<div className='ChatRooms__left__title'>Group 03</div>
+				<div className='ChatRooms__createButton'>
 					<button
 						onClick={() => setShowModel(true)}
-						className='ChatRooms__left__button'
+						className='ChatRooms__createButton__button'
 					>
-						Create new Discord <FaPlus style={{marginLeft: "5px"}} />
+						Create Discord <FaPlus style={{marginLeft: "5px"}} />
 					</button>
-				</div>
-				<div className='ChatRooms__right'>
-					<div className='ChatRooms__right__title'>DISCORD NAME : Group 01</div>
-					<div className='ChatRooms__right__contents'>
-						<div className='ChatRooms__right__content'>
-							<div className='ChatRooms__right__content__user'>user 01</div>
-							<div className='ChatRooms__right__content__message'>
-								Hey welcome to Temporary Discord
-							</div>
-							<div className='ChatRooms__right__content__date'>
-								20-1-2020 5.45pm
-							</div>
-						</div>
-						<div className='ChatRooms__right__content'>
-							<div className='ChatRooms__right__content__user'>user 01</div>
-							<div className='ChatRooms__right__content__message'>
-								Hey welcome to Temporary Discord
-							</div>
-							<div className='ChatRooms__right__content__date'>
-								20-1-2020 5.45pm
-							</div>
-						</div>
-						<div className='ChatRooms__right__content'>
-							<div className='ChatRooms__right__content__user'>user 01</div>
-							<div className='ChatRooms__right__content__message'>
-								Hey welcome to Temporary Discord
-							</div>
-							<div className='ChatRooms__right__content__date'>
-								20-1-2020 5.45pm
-							</div>
-						</div>
-					</div>
-					<div className='ChatRooms__right__input'>
-						<input
-							value={message}
-							onChange={(e) => setIntput({...input, message: e.target.value})}
-							type='text'
-							className='ChatRooms__right__input__field'
-							placeholder='Type a message'
-						/>
-						<button className='ChatRooms__right__input__button'>
-							<FaArrowRight />
-						</button>
-					</div>
 				</div>
 			</div>
 			<Modal
@@ -117,6 +65,7 @@ function ChatRooms() {
 						value={chatRoom}
 						onChange={(e) => setIntput({...input, chatRoom: e.target.value})}
 						type='text'
+						placeholder='Rocky Baby !!'
 						className='ChatRooms__model__input'
 					/>
 					<button
